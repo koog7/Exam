9d7f22debb21c831615248a4f885ab689050bd1b
koog7 <k.ognev709@mail.com>
2024-09-21 00:31:01 +0600

    Подключен axios , Настроен redux-persist

---
+++ frontend/src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "../components/Navbar.tsx";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Home;
