4b22207f82689c506b13367757a6b2b93bdcfda0
koog7 <k.ognev709@mail.com>
2024-09-21 09:53:09 +0600

    Добавлено роут для логаута

--- backend/routes/UserRouters.ts
+++ backend/routes/UserRouters.ts
@@ -58,4 +58,26 @@ UserRouter.post('/sessions' , async (req , res , next) => {
     }
 })
 
+ForumUserRouter.delete('/sessions', async (req, res, next) => {
+    try {
+        const getToken = req.get('Authorization');
+        const success = {message: 'Success'};
+
+        if (!getToken) return res.send(success);
+
+        const [_Bearer , token] = getToken.split(' ');
+
+        const user = await User.findOne({token});
+
+        if (!user) return res.send(success);
+
+        user.token = randomUUID();
+        user.save();
+
+        return res.send(success);
+    } catch (e) {
+        return next(e);
+    }
+});
+
 export default UserRouter;
