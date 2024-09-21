27ba679d0a11e5338f5f3d9f5030576c5a53c40b
koog7 <k.ognev709@mail.com>
2024-09-21 12:25:36 +0600

    Написан роут для создания поста о товаре

--- backend/models/Product.ts
+++ backend/models/Product.ts
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
+    userId:{
+        type: String,
+        required:true
+    },
     title:{
         type: String,
         required:true
--- backend/routes/ProductRouters.ts
+++ backend/routes/ProductRouters.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import Product from "../models/Product";
+import {imagesUpload} from "../multer";
+import User from "../models/User";
 
 
 const ProductRouter = express.Router();
@@ -14,6 +16,38 @@ ProductRouter.get('/' , async (req ,res , next )=>{
         }
 
         res.send(getAllProduct)
+    }catch (e) {
+        next(e)
+    }
+})
+
+ProductRouter.post('/', imagesUpload.single('image') ,async (req , res , next ) =>{
+    const getToken = req.get('Authorization');
+
+    if(!getToken){
+        return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    try {
+        const [_Bearer, token] = getToken.split(' ');
+
+        const UserData = await User.findOne({token: token})
+
+        if (!UserData || !UserData._id) {
+            return res.status(400).send({ error: 'User not found' });
+        }
+
+        const newProduct = new Product({
+            userId: UserData._id,
+            title: req.body.title,
+            description: req.body.description,
+            price:req.body.price,
+            image: req.file?.filename,
+            category: req.body.category,
+        })
+
+        await newProduct.save()
+        res.send(newProduct)
     }catch (e) {
         next(e)
     }
