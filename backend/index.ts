4db782fa58d3a4331529f327c134ff172e7bb997
koog7 <k.ognev709@mail.com>
2024-09-21 09:51:48 +0600

    Добавлен cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from "mongoose";
 import UserRouter from "./routes/UserRouters";
+import cors from 'cors';
 
 const app = express()
 const port = 8000;
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use('/users' , UserRouter)
